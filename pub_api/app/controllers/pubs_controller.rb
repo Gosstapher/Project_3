e6b32996b8a10557b7f95467605aed9cdd46c93b
Gosstapher <gosstapher@gmail.com>
2016-03-06 17:27:06 +0000

    pub model and controller scaffolded, json objects rendered correctly in browser

---
+++ pub_api/app/controllers/pubs_controller.rb
@@ -0,0 +1,59 @@
+class PubsController < ApplicationController
+  before_action :set_pub, only: [:show, :update, :destroy]
+
+  # GET /pubs
+  # GET /pubs.json
+  def index
+    @pubs = Pub.all
+
+    render json: @pubs
+  end
+
+  # GET /pubs/1
+  # GET /pubs/1.json
+  def show
+    render json: @pub
+  end
+
+  # POST /pubs
+  # POST /pubs.json
+  def create
+    @pub = Pub.new(pub_params)
+
+    if @pub.save
+      render json: @pub, status: :created, location: @pub
+    else
+      render json: @pub.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /pubs/1
+  # PATCH/PUT /pubs/1.json
+  def update
+    @pub = Pub.find(params[:id])
+
+    if @pub.update(pub_params)
+      head :no_content
+    else
+      render json: @pub.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /pubs/1
+  # DELETE /pubs/1.json
+  def destroy
+    @pub.destroy
+
+    head :no_content
+  end
+
+  private
+
+    def set_pub
+      @pub = Pub.find(params[:id])
+    end
+
+    def pub_params
+      params.require(:pub).permit(:name, :location, :details)
+    end
+end
---
+++ pub_api/test/controllers/pubs_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class PubsControllerTest < ActionController::TestCase
+  setup do
+    @pub = pubs(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:pubs)
+  end
+
+  test "should create pub" do
+    assert_difference('Pub.count') do
+      post :create, pub: { details: @pub.details, location: @pub.location, name: @pub.name }
+    end
+
+    assert_response 201
+  end
+
+  test "should show pub" do
+    get :show, id: @pub
+    assert_response :success
+  end
+
+  test "should update pub" do
+    put :update, id: @pub, pub: { details: @pub.details, location: @pub.location, name: @pub.name }
+    assert_response 204
+  end
+
+  test "should destroy pub" do
+    assert_difference('Pub.count', -1) do
+      delete :destroy, id: @pub
+    end
+
+    assert_response 204
+  end
+end
